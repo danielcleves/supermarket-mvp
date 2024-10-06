b5b82f1397da41aa09162859d3f73ed18c0b008c
danielcleves <daniel.cleves.rueda@gmail.com>
2024-10-06 18:45:18 -0500

    Se adiciona al formulario la interfaz IPayModeView

--- Supermarket-mvp/Views/PayModeView.cs
+++ Supermarket-mvp/Views/PayModeView.cs
@@ -10,11 +10,31 @@ using System.Windows.Forms;
 
 namespace Supermarket_mvp.Views
 {
-    public partial class PayModeView : Form
+    public partial class PayModeView : Form, IPayModeView
     {
         public PayModeView()
         {
             InitializeComponent();
+        }
+
+        public string PayModeId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string PayModeName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string PayModeObservation { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string SearchValue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsEdit { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsSuccessful { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Message { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public event EventHandler SearchEvent;
+        public event EventHandler AddNewEvent;
+        public event EventHandler EditEvent;
+        public event EventHandler DeleteEvent;
+        public event EventHandler SaveEvent;
+        public event EventHandler CancelEvent;
+
+        public void SetPayModeListBildingSource(BindingSource payModeList)
+        {
+            throw new NotImplementedException();
         }
     }
 }
