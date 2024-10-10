1e7917115ad0adbc843cc527561fc05576c6eb53
danielcleves <daniel.cleves.rueda@gmail.com>
2024-10-09 22:54:53 -0500

    Agregar implementación CategoryRepository para manejar las operaciones CRUD de categorías

---
+++ Supermarket-mvp/_Repositories/CategoryRepository.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Data.SqlClient;
+using Supermarket_mvp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp._Repositories
+{
+    internal class CategoryRepository : BaseRepository, ICategoryRepository
+    {
+        public CategoryRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void Add(CategoryModel categoryModel)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "INSERT INTO Categories (name, description) VALUES (@name, @description)";
+                command.Parameters.Add("@name", SqlDbType.Char).Value = categoryModel.Name;
+                command.Parameters.Add("@description", SqlDbType.Char).Value = categoryModel.Description;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "DELETE FROM Categories WHERE id = @id";
+                command.Parameters.Add("@id", SqlDbType.Int).Value = id;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Edit(CategoryModel categoryModel)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"UPDATE Categories
+                                    SET name = @name,
+                                        description = @description
+                                    WHERE id = @id";
+                command.Parameters.Add("@name", SqlDbType.Char).Value = categoryModel.Name;
+                command.Parameters.Add("@description", SqlDbType.Char).Value = categoryModel.Description;
+                command.Parameters.Add("@id", SqlDbType.Int).Value = categoryModel.Id;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public IEnumerable<CategoryModel> GetAll()
+        {
+            var categoryList = new List<CategoryModel>();
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM Categories ORDER BY id DESC";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var categoryModel = new CategoryModel();
+                        categoryModel.Id = (int)reader["id"];
+                        categoryModel.Name = reader["name"].ToString();
+                        categoryModel.Description = reader["description"].ToString();
+                        categoryList.Add(categoryModel);
+                    }
+                }
+            }
+            return categoryList;
+        }
+
+        public IEnumerable<CategoryModel> GetByValue(string value)
+        {
+            var categoryList = new List<CategoryModel>();
+            int categoryId = int.TryParse(value, out _) ? Convert.ToInt32(value) : 0;
+            string categoryName = value;
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"SELECT * FROM Categories
+                                    WHERE id = @id OR name LIKE @name + '%'
+                                    ORDER BY id DESC";
+                command.Parameters.Add("@id", SqlDbType.Int).Value = categoryId;
+                command.Parameters.Add("@name", SqlDbType.Char).Value = categoryName;
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var categoryModel = new CategoryModel();
+                        categoryModel.Id = (int)reader["id"];
+                        categoryModel.Name = reader["name"].ToString();
+                        categoryModel.Description = reader["description"].ToString();
+                        categoryList.Add(categoryModel);
+                    }
+                }
+            }
+            return categoryList;
+        }
+    }
+
+}
