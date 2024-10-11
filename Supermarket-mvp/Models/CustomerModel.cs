411c564ce5ee1387f18c7fed187ffe7d7ae3f81b
danielcleves <daniel.cleves.rueda@gmail.com>
2024-10-10 19:37:26 -0500

    Se creó el modelo CustomerModel para la tabla Customers

---
+++ Supermarket-mvp/Models/CustomerModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Models
+{
+    internal class CustomerModel
+    {
+        [DisplayName("Customer Id")]
+        public int Id { get; set; }
+
+        [DisplayName("Document Number")]
+        [Required(ErrorMessage = "Document number is required")]
+        [StringLength(15, MinimumLength = 3, ErrorMessage = "Document number must be between 3 and 15 characters")]
+        public string DocumentNumber { get; set; }
+
+        [DisplayName("First Name")]
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "First name must be between 1 and 50 characters")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        [Required(ErrorMessage = "Last name is required")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = "Last name must be between 1 and 50 characters")]
+        public string LastName { get; set; }
+
+        [DisplayName("Address")]
+        [StringLength(80, ErrorMessage = "Address must be a maximum of 80 characters")]
+        public string? Address { get; set; }
+
+        [DisplayName("Birthday")]
+        [DataType(DataType.Date)]
+        public DateTime? Birthday { get; set; }
+
+        [DisplayName("Phone Number")]
+        [StringLength(16, ErrorMessage = "Phone number must be a maximum of 16 characters")]
+        public string? PhoneNumber { get; set; }
+
+        [DisplayName("Email")]
+        [StringLength(100, ErrorMessage = "Email must be a maximum of 100 characters")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string? Email { get; set; }
+    }
+}
