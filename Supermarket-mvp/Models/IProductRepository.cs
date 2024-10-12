cccb2cd7b7053d0d59a7a9ee8c1b6061fbb531e4
danielcleves <daniel.cleves.rueda@gmail.com>
2024-10-12 12:17:11 -0500

    feat: Implementación de la interfaz IProductRepository

---
+++ Supermarket-mvp/Models/IProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Models
+{
+    internal interface IProductRepository
+    {
+        internal interface IProductRepository
+        {
+            void Add(ProductModel productModel);           // Agregar un nuevo producto
+            void Edit(ProductModel productModel);          // Editar un producto existente
+            void Delete(int id);                           // Eliminar un producto por ID
+            IEnumerable<ProductModel> GetAll();            // Obtener todos los productos
+            IEnumerable<ProductModel> GetByValue(string value);  // Buscar productos
+        }
+
+    }
+}
