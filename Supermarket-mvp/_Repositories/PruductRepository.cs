c61cee10f383eda527592ab5a1079d7699b63408
danielcleves <daniel.cleves.rueda@gmail.com>
2024-10-12 12:24:58 -0500

    feat: Implement ProductRepository for CRUD operations

---
+++ Supermarket-mvp/_Repositories/PruductRepository.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.Data.SqlClient;
+using Supermarket_mvp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp._Repositories
+{
+    internal class ProductRepository : BaseRepository, IProductRepository
+    {
+        public ProductRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void Add(ProductModel productModel)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "INSERT INTO Products (name, price, stock, category_id) VALUES (@name, @price, @stock, @categoryId)";
+                command.Parameters.Add("@name", SqlDbType.NChar).Value = productModel.Name;
+                command.Parameters.Add("@price", SqlDbType.Int).Value = productModel.Price;
+                command.Parameters.Add("@stock", SqlDbType.Int).Value = productModel.Stock;
+                command.Parameters.Add("@categoryId", SqlDbType.Int).Value = productModel.CategoryId;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "DELETE FROM Products WHERE id = @id";
+                command.Parameters.Add("@id", SqlDbType.Int).Value = id;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void Edit(ProductModel productModel)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"UPDATE Products
+                                    SET name = @name,
+                                        price = @price,
+                                        stock = @stock,
+                                        category_id = @categoryId
+                                    WHERE id = @id";
+                command.Parameters.Add("@name", SqlDbType.NChar).Value = productModel.Name;
+                command.Parameters.Add("@price", SqlDbType.Int).Value = productModel.Price;
+                command.Parameters.Add("@stock", SqlDbType.Int).Value = productModel.Stock;
+                command.Parameters.Add("@categoryId", SqlDbType.Int).Value = productModel.CategoryId;
+                command.Parameters.Add("@id", SqlDbType.Int).Value = productModel.Id;
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public IEnumerable<ProductModel> GetAll()
+        {
+            var productList = new List<ProductModel>();
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM Products ORDER BY id DESC";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var productModel = new ProductModel();
+                        productModel.Id = (int)reader["id"];
+                        productModel.Name = reader["name"].ToString();
+                        productModel.Price = (int)reader["price"];
+                        productModel.Stock = (int)reader["stock"];
+                        productModel.CategoryId = (int)reader["category_id"];
+                        productList.Add(productModel);
+                    }
+                }
+            }
+            return productList;
+        }
+
+        public IEnumerable<ProductModel> GetByValue(string value)
+        {
+            var productList = new List<ProductModel>();
+            int productId = int.TryParse(value, out _) ? Convert.ToInt32(value) : 0;
+            string productName = value;
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"SELECT * FROM Products
+                                    WHERE id = @id OR name LIKE @name + '%'
+                                    ORDER BY id DESC";
+                command.Parameters.Add("@id", SqlDbType.Int).Value = productId;
+                command.Parameters.Add("@name", SqlDbType.NChar).Value = productName;
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var productModel = new ProductModel();
+                        productModel.Id = (int)reader["id"];
+                        productModel.Name = reader["name"].ToString();
+                        productModel.Price = (int)reader["price"];
+                        productModel.Stock = (int)reader["stock"];
+                        productModel.CategoryId = (int)reader["category_id"];
+                        productList.Add(productModel);
+                    }
+                }
+            }
+            return productList;
+        }
+    }
+
+}
